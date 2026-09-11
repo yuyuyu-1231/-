8071ab92b97e8c4d6c45b9a471e0637e059a10c8
yuyuyu-1231 <1359497348@qq.com>
2026-09-11 15:35:15 +0800

    更新贪心算法.py：新增最大容量问题解法（双指针贪心）

--- 贪心算法.py
+++ 贪心算法.py
@@ -1,15 +1,61 @@
-def coin_change_greedy(coins: list[int],amt:int):
-    i = len(coins) - 1
-    count = 0
-    while amt > 0:
-        while i > 0 and coins[i] > amt:
-            i -= 1
-        amt -= coins[i]
-        count += 1
-    return count if amt == 0 else -1
+# def coin_change_greedy(coins: list[int],amt:int):
+#     i = len(coins) - 1
+#     count = 0
+#     while amt > 0:
+#         while i > 0 and coins[i] > amt:
+#             i -= 1
+#         amt -= coins[i]
+#         count += 1
+#     return count if amt == 0 else -1
+#
+# if __name__ == '__main__':
+#     coins = [1, 2, 5]
+#     amt = 11
+#     print(coin_change_greedy(coins, amt))
 
-if __name__ == '__main__':
-    coins = [1, 2, 5]
-    amt = 11
-    print(coin_change_greedy(coins, amt))
+#分数背包问题
+# class Item:
+#     def __init__(self, w:int,v:int):
+#         self.w = w #weight
+#         self.v = v #value
+#
+# def fractional_knapsack(wgt: list[int], val: list[int], cap: int)-> int:
+#     items = [Item(w,v) for w,v in zip(wgt,val)]
+#     items.sort(key=lambda item: item.v / item.w, reverse=True)
+#     res = 0
+#     for item in items:
+#         if cap >= item.w:
+#             cap -= item.w
+#             res += item.v
+#         else:
+#             res += item.v * cap / item.w
+#             break
+#     return res
+#
+#
+#
+# if __name__ == "__main__":
+#     wgt = [10,20,30,40,50]
+#     val = [50,120,150,210,240]
+#     cap = 50
+#     n = len(wgt)
+#
+#     res = fractional_knapsack(wgt,val,cap)
+#     print(res)
 
+#最大容量问题
+def max_capacity(ht: list[int])->int:
+    i,j = 0,len(ht)-1
+    res = 0
+    while i < j :
+        cap = min(ht[i],ht[j])*(j-1)
+        res = max(res,cap)
+        if ht[i] < ht[j]:
+            i += 1
+        else:
+            j -= 1
+    return res
+if __name__ == "__main__":
+    ht = [3,8,5,2,7,7,3,4]
+    res = max_capacity(ht)
+    print(res)
