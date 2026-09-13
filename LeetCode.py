17f8be3630f6a19fe94f2aab6ac42be45b5f2cbc
yuyuyu-1231 <1359497348@qq.com>
2026-09-13 15:20:01 +0800

    更新 LeetCode.py：新增合并有序链表与反转链表题解

--- LeetCode.py
+++ LeetCode.py
@@ -9,3 +9,64 @@ class Solution:
             dic[s[j]] = j
             res = max(res,j - i)
         return res
+#动态规划+哈希表
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        dic = {}
+        res = tmp = 0
+        for j in range(len(s)):
+            i = dic.get(s[j],-1)
+            dic[s[j]]=j
+            tmp = tmp + 1 if tmp < j-i else j-i
+            res = max(res,tmp)
+        return res
+
+#合并两个有序链表
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        cur = dum = ListNode(0)
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next , list1 = list1 , list1.next
+            else:
+                cur.next ,list2 = list2 , list2.next
+            cur = cur.next
+        cur.next = list1 if list1 else list2
+        return dum.next
+
+#反转列表（迭代）
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur , pre = head , None
+        while cur:
+            tmp = cur.next
+            cur.next = pre
+            pre = cur
+            cur =tmp
+        return pre
+
+#反转列表（递归）
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def recur(cur,pre):
+            if not cur: return pre
+            res = recur(cur.next,cur)
+            cur.next = pre
+            return res
+
+        return recur(head,None)
