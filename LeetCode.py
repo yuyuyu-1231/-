4afc438956c720516439bfb9c430549e68284901
yuyuyu-1231 <1359497348@qq.com>
2026-09-14 13:59:53 +0800

    更新 LeetCode.py：新增分隔链表、删除节点与随机链表复制题解

--- LeetCode.py
+++ LeetCode.py
@@ -70,3 +70,58 @@ class Solution:
             return res
 
         return recur(head,None)
+
+#分隔链表
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        sml_dummy,big_dummy = ListNode(0),ListNode(0)
+        sml ,big = sml_dummy , big_dummy
+        while head:
+            if head.val < x:
+                sml.next = head
+                sml = sml.next
+            else:
+                big.next = head
+                big = big.next
+            head = head.next
+        sml.next = big_dummy.next
+        big.next = None
+        return sml_dummy.next
+
+#删除链表中的节点
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+
+#随机链表的复制
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head: return
+        dic = {}
+        cur = head
+        while cur:
+            dic[cur] = Node(cur.val)
+            cur = cur.next
+        cur = head
+        while cur:
+            dic[cur].next = dic.get(cur.next)
+            dic[cur].random = dic.get(cur.random)
+            cur = cur.next
+        return dic[head]
